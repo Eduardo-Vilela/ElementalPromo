c4afa30c6ca8b77e54f269d72fbfb7754f119609
eduardo <you@example.com>
2022-06-01 18:02:56 -0300

    comienzo

---
+++ src/Landing/ElementalPromo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BoxPrincipal,BoxSecundario } from './Elements'
+
+
+function ElementalPromo() {
+  return (
+    <>
+     <BoxPrincipal>
+         <BoxSecundario>
+         </BoxSecundario>
+     </BoxPrincipal>
+    </>
+  )
+}
+
+export default ElementalPromo
---
+++ src/Landing/Elements.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const BoxPrincipal = styled.div`
+width: 100%;
+height: 1000px;
+background: grey;
+background-image: url(https://res.cloudinary.com/grupo-delsud/image/upload/v1654116683/ELEMENTAL%20CONSTRUCTORA/LandingPromo/FONDO_Mesa_de_trabajo_1_vgcwxc.png);
+background-size: cover;
+`
+
+export const BoxSecundario = styled.div`
+width: 80%;
+height: auto;
+`
