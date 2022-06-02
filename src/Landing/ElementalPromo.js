cd44712008c11d0ca4ebe32562855bddd7a83d1e
eduardo <you@example.com>
2022-06-02 18:05:16 -0300

    falta formulario

--- src/Landing/ElementalPromo.js
+++ src/Landing/ElementalPromo.js
@@ -1,14 +1,167 @@
 import React from 'react'
-import { BoxPrincipal,BoxSecundario } from './Elements'
+import { BoxPrincipal,
+  BoxSecundario,
+  BoxDerecha,
+  BoxIzquierda,
+  BoxAbajo,
+  BoxArriba,
+  Box1,
+  Box2,
+  Parte1,
+  Parte2,
+  BoxTitulo,
+  BoxCarrousel,
+  BoxParrafos,
+  BoxCasita,
+  BoxFamilias,
+  Titulo,
+  Container,
+  Parrafo1,
+  Parrafo2,
+  Parrafo3,
+  SubBoxCarusel,
+  TituloParrafos,
+  TextoParrafo,
+  Boxvacio,
+  Boxmapa,
+  BoxBeneficios,
+  Beneficio,
+  Icono,
+  Subtitulo,
+  FotoPlano,
+  TituloCasita,
+  Texto2,
 
+} from './Elements'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import "swiper/css/pagination";
+import "swiper/css";
 
 function ElementalPromo() {
   return (
     <>
+    <Container>
      <BoxPrincipal>
          <BoxSecundario>
+           <BoxDerecha>
+
+           </BoxDerecha>
+           <BoxIzquierda>
+              <BoxArriba>
+                <Box1>
+
+                </Box1>
+                <Box2>
+                  <Parte1>
+                    <BoxTitulo>
+                      <Titulo>
+                        EL SUEÑO DE LA CASA PROPIA, EN TIEMPO RECORD.<br/> 
+                        <b>FINANCIACION A TASA 0% EN PESOS</b>
+                      </Titulo>
+                    </BoxTitulo>
+                    <BoxCarrousel>
+                      <SubBoxCarusel>
+                        <Swiper
+                          spaceBetween={30}
+                          autoplay={{
+                            delay: 1500,
+                            disableOnInteraction: false,
+                          }}
+                          modules={[Autoplay]}
+                          className="mySwiper"
+                        >
+                          <SwiperSlide><img alt='slide1' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654116682/ELEMENTAL%20CONSTRUCTORA/LandingPromo/SLIDE_1_ycr9zn.webp'/> </SwiperSlide>
+                          <SwiperSlide><img alt='slide2' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654116682/ELEMENTAL%20CONSTRUCTORA/LandingPromo/SLIDE_2_kupyap.webp'/></SwiperSlide>
+                          <SwiperSlide><img alt='slide3'src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654116682/ELEMENTAL%20CONSTRUCTORA/LandingPromo/SLIDE_3_biavxn.webp'/></SwiperSlide>
+                          <SwiperSlide><img alt='slide4' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654116682/ELEMENTAL%20CONSTRUCTORA/LandingPromo/SLIDE_4_nfpplc.webp'/></SwiperSlide>
+                          <SwiperSlide><img alt='slide5' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654116682/ELEMENTAL%20CONSTRUCTORA/LandingPromo/SLIDE_5_qgfie7.webp'/></SwiperSlide>
+                        </Swiper>
+                      </SubBoxCarusel>
+                    </BoxCarrousel>
+                    <BoxParrafos>
+                      <Parrafo1>
+                         <TituloParrafos>Línea Modular</TituloParrafos>
+                         <TextoParrafo>
+                           Vivienda super económica con entrega en sólo 60 días 
+                           y una colocación de sólo 72hs, manteniendo una alta 
+                           calidad de realización y respetando los plazos pactados.
+                         </TextoParrafo>
+                      </Parrafo1>
+                      <Parrafo2>
+                         <TituloParrafos>Línea tradicional</TituloParrafos>
+                         <TextoParrafo> 
+                           Ofrecemos una variedad de modelos 
+                           preestablecidos de casas y dúplex, 
+                           con la posibilidad de personalizarlos
+                            completamente a tu medida.
+                         </TextoParrafo>
+                      </Parrafo2>
+                      <Parrafo3>
+                         <TituloParrafos>Línea design</TituloParrafos>
+                         <TextoParrafo>
+                            Para satisfacer a todo nuestro público 
+                            ofrecemos un servicio de construcción 
+                            completamente a tu medida, traenos tu 
+                            idea y nosotros la materializamos 
+                            como la soñaste.
+                         </TextoParrafo>
+                      </Parrafo3>
+                    </BoxParrafos>
+                  </Parte1>
+                  <Parte2>
+                    <BoxCasita>
+                       <img width='50' height='50' alt='icono' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654202015/ELEMENTAL%20CONSTRUCTORA/LandingPromo/Frame_ullzbf.png'/>
+                       <img alt='linea' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654202690/ELEMENTAL%20CONSTRUCTORA/LandingPromo/linea_hmr0je.png'/>
+                       <TituloCasita>
+                        Construimos la casa de  tus 
+                        sueños a tu medida, 
+                        con la mejor financiación.
+                       </TituloCasita>
+                    </BoxCasita>
+                    <BoxFamilias>
+                       <Texto2>18</Texto2>
+                       <Texto2>FAMILIAS NOS <br/>ELIGIERON</Texto2>
+                       <Texto2>84</Texto2>
+                       <Texto2>EN PROMEDIO MENSUAL <br/>DE FORMULARIOS <br/>RECIBIDOS</Texto2>
+                    </BoxFamilias>
+                    <div>
+
+                    </div>
+                  </Parte2>
+                 
+                </Box2>
+              </BoxArriba>
+              <BoxAbajo>
+                <Boxvacio>
+
+                </Boxvacio>
+                <BoxBeneficios>
+                   <Beneficio>
+                     <Icono alt='icono1' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654198416/ELEMENTAL%20CONSTRUCTORA/LandingPromo/Group_visoou.png'/>
+                     <Subtitulo>Rápida <br/> realización</Subtitulo> 
+                   </Beneficio>
+                   <Beneficio>
+                     <Icono alt='icono2' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654198416/ELEMENTAL%20CONSTRUCTORA/LandingPromo/icono2_qxpega.png'/>
+                     <Subtitulo>Valores <br/> Competitivos</Subtitulo> 
+                   </Beneficio>
+                   <Beneficio>
+                     <Icono alt='icono3' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654198416/ELEMENTAL%20CONSTRUCTORA/LandingPromo/icono4_zv8ysi.png'/>
+                     <Subtitulo>Financiación <br/> Personalizada</Subtitulo> 
+                   </Beneficio>
+                   <Beneficio>
+                     <Icono alt='icono4' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654198416/ELEMENTAL%20CONSTRUCTORA/LandingPromo/icono3_z8hehr.png'/>
+                     <Subtitulo>Variedad  <br/> De Servicios</Subtitulo> 
+                   </Beneficio>
+                </BoxBeneficios>
+                <Boxmapa>
+                   <FotoPlano alt='planocasa' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1654116683/ELEMENTAL%20CONSTRUCTORA/LandingPromo/imagen_1-24-min_ajpd4b.png'/>
+                </Boxmapa>
+              </BoxAbajo>
+           </BoxIzquierda>
          </BoxSecundario>
      </BoxPrincipal>
+     </Container>
     </>
   )
 }
--- src/Landing/Elements.js
+++ src/Landing/Elements.js
@@ -1,14 +1,236 @@
 import styled from 'styled-components'
 
+export const Container = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300&display=swap');
+  
+  
+    & h1 {
+        font-family: 'Hind Siliguri', sans-serif;
+    }
+  
+    & h2 {
+        font-family: 'Hind Siliguri', sans-serif;
+    }
+    & h3 {
+        font-family: 'Hind Siliguri', sans-serif;
+
+    }
+    body {
+        font-family: 'Hind Siliguri', sans-serif;
+    }
+`
+
+
+
 export const BoxPrincipal = styled.div`
 width: 100%;
 height: 1000px;
-background: grey;
-background-image: url(https://res.cloudinary.com/grupo-delsud/image/upload/v1654116683/ELEMENTAL%20CONSTRUCTORA/LandingPromo/FONDO_Mesa_de_trabajo_1_vgcwxc.png);
+background: white;
+background-image: url(https://res.cloudinary.com/grupo-delsud/image/upload/v1654171604/ELEMENTAL%20CONSTRUCTORA/LandingPromo/FONDO_WEB_dydqou.png);
 background-size: cover;
 `
 
 export const BoxSecundario = styled.div`
+width: 100%;
+height: 1000px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+margin: auto;
+position: relative;
+`
+
+export const BoxDerecha = styled.div`
+width: 25%;
+height: 1000px; 
+background:#F5F2F2;
+position: absolute;
+top: 0;
+bottom: 0;
+left: 80px;
+right: 0;
+`
+export const BoxIzquierda = styled.div`
+width: 100%;
+height: 1000px;  
+`
+
+export const BoxAbajo = styled.div`
+width: 100%;
+height: 300px;  
+background-color: #1A1A1A;
+display: flex;
+flex-direction: row;
+justify-content: center;
+`
+export const BoxArriba = styled.div`
+width: 100%;
+height: 700px;  
+display: flex;
+flex-direction: row;
+justify-content: center;
+`
+export const Box1 = styled.div`
+width: 30%;
+height: 700px;
+
+`
+export const Box2 = styled.div`
+width: 70%;
+height: 700px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+`
+export const Parte1 = styled.div`
+width: 70%;
+height: 700px;
+`
+export const Parte2 = styled.div`
+  
+height:60vh;
+width: 18vw;
+position:relative;
+margin: auto;
+display: flex;
+flex-direction:column;
+justify-content: center;
+
+ &:after{
+    position: absolute;
+    top: 0%;
+    left: 0;
+    content: '';
+    height: 290px;
+    width: 0;
+    border-left: 18vw solid #1A1A1A;
+    border-bottom: 8vw solid transparent;
+    border-top: 0px solid transparent;
+    overflow: hidden;
+  }
+`
+export const BoxTitulo = styled.div`
+width: 100%;
+height: 70px;
+`
+export const BoxCarrousel = styled.div`
+width: 100%;
+height: 400px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+`
+export const BoxParrafos = styled.div`
 width: 80%;
-height: auto;
+height: 200px;
+margin: auto;
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+`
+export const BoxCasita = styled.div`
+width: 90%;
+height: 520px;
+margin: auto;
+z-index: 9999;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+text-align: center;
+align-items: center;
+`
+
+export const BoxFamilias = styled.div`
+width: 90%;
+height: 460px;
+margin: auto;
+border: 1px solid #000000;
+`
+export const Titulo = styled.p`
+font-weight: 400;
+font-size: 27px;
+line-height: 33px;
+font-family: 'Hind Siliguri', sans-serif;
+text-align: center;
+`
+export const Parrafo1 = styled.div`
+width: 25%;
+height: 200px;
+
+`
+export const Parrafo2 = styled.div`
+width: 25%;
+height: 200px;
+`
+export const Parrafo3 = styled.div`
+width: 25%;
+height: 200px;
+`
+export const SubBoxCarusel = styled.div`
+width: 80%;
+height: 400px;
+`
+export const TituloParrafos = styled.h3`
+font-family: 'Hind Siliguri', sans-serif;
+font-size: 17px;
+`
+export const TextoParrafo = styled.p`
+font-family: 'Hind Siliguri', sans-serif;
+font-size: 11px;
+font-weight: 600;
+
+`
+export const Boxmapa = styled.div`
+width: 50%;
+height:300px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+`
+export const BoxBeneficios = styled.div`
+width: 25%;
+height:300px;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+`
+
+export const Boxvacio = styled.div`
+width: 35%;
+height:300px;
+`
+export const Beneficio = styled.div`
+width: 180px;
+height: 125px;
+text-align: center;
+align-self: end;
+`
+
+export const Icono = styled.img`
+
+`
+export const Subtitulo = styled.p`
+font-family: 'Hind Siliguri', sans-serif;
+font-size: 17px;
+color: white;
+line-height: 15px;
+font-weight: 500;
+`
+export const FotoPlano = styled.img`
+width: 750px;
+height: 250px;
+`
+
+export const TituloCasita = styled.h3`
+color: white;
+font-family: 'Hind Siliguri', sans-serif;
+font-size: 22px;
+font-weight: 200;
+`
+
+export const Texto2 = styled.p`
+font-size: 12px;
+color: black;
+font-weight: 200;
+font-family: 'Hind Siliguri', sans-serif;
 `
